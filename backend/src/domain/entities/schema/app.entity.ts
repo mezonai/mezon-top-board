50ee149a28e5ce0dc5dc28d7e26056b0eca59510
Duke <tuoc31082004@gmail.com>
2025-10-16 11:01:40 +0700

    MTB-260-commit-fixed-prettierformat

--- backend/src/domain/entities/schema/app.entity.ts
+++ backend/src/domain/entities/schema/app.entity.ts
@@ -1,96 +1,72 @@
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  JoinTable,
-  ManyToMany,
-  ManyToOne,
-  OneToMany,
-} from "typeorm";
+import { Column, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, PrimaryColumn, Unique } from "typeorm";
 
-import { AppPricing } from "@domain/common/enum/appPricing";
 import { AppStatus } from "@domain/common/enum/appStatus";
-import { MezonAppType } from "@domain/common/enum/mezonAppType";
 import { Link, AppReviewHistory, Rating, Tag, User } from "@domain/entities";
 
 import { BaseSoftDelete } from "../base";
+import { MezonAppType } from "@domain/common/enum/mezonAppType";
 
 @Entity()
 export class App extends BaseSoftDelete {
-  @Column()
-  public name: string;
+    @Column()
+    public name: string;
 
-  @Column()
-  public ownerId: string;
+    @Column()
+    public ownerId: string;
 
-  @Column({
-    type: "enum",
-    enum: AppStatus,
-    default: AppStatus.PENDING,
-  })
-  public status: AppStatus;
+    @Column({
+        type: "enum",
+        enum: AppStatus,
+        default: AppStatus.PENDING,
+    })
+    public status: AppStatus;
 
-  @Column({ default: false })
-  public isAutoPublished: boolean;
+    @Column({ default: false })
+    public isAutoPublished: boolean;
 
-  @Column({ nullable: true })
-  public mezonAppId: string;
+    @Column({ nullable: true })
+    public mezonAppId: string;
 
-  @Column({
-    type: "enum",
-    enum: MezonAppType,
-    default: MezonAppType.BOT,
-  })
-  public type: MezonAppType;
+    @Column({
+        type: "enum",
+        enum: MezonAppType,
+        default: MezonAppType.BOT,
+    })
+    public type: MezonAppType;
 
-  @Column({ nullable: true })
-  public headline: string;
+    @Column({ nullable: true })
+    public headline: string;
 
-  @Column({ nullable: true })
-  public description: string;
+    @Column({ nullable: true })
+    public description: string;
 
-  @Column({ nullable: true })
-  public prefix: string;
+    @Column({ nullable: true })
+    public prefix: string;
 
-  @Column({ nullable: true })
-  public featuredImage: string;
+    @Column({ nullable: true })
+    public featuredImage: string;
 
-  @Column({ nullable: true })
-  public supportUrl: string;
+    @Column({ nullable: true })
+    public supportUrl: string;
 
-  @Column({ nullable: true })
-  public remark: string;
+    @Column({ nullable: true })
+    public remark: string;
 
-  @Column({
-    type: "enum",
-    enum: AppPricing,
-    default: AppPricing.FREE,
-  })
-  public pricingTag: AppPricing;
+    @ManyToMany(() => Tag, (tag) => tag.apps)
+    @JoinTable()
+    public tags: Tag[];
 
-  @Column({
-    type: "decimal",
-    precision: 15,
-    scale: 0,
-    nullable: true,
-  })
-  public price: number | null;
+    @ManyToMany(() => Link, (link) => link.apps)
+    @JoinTable()
+    public socialLinks: Link[];
 
-  @ManyToMany(() => Tag, (tag) => tag.apps)
-  @JoinTable()
-  public tags: Tag[];
+    @OneToMany(() => AppReviewHistory, (review) => review.app)
+    reviewHistories: AppReviewHistory[];
 
-  @ManyToMany(() => Link, (link) => link.apps)
-  @JoinTable()
-  public socialLinks: Link[];
+    @OneToMany(() => Rating, (rating) => rating.app)
+    ratings: Rating[];
 
-  @OneToMany(() => AppReviewHistory, (review) => review.app)
-  reviewHistories: AppReviewHistory[];
-
-  @OneToMany(() => Rating, (rating) => rating.app)
-  ratings: Rating[];
-
-  @ManyToOne(() => User, (user) => user.apps, { onDelete: "CASCADE" })
-  @JoinColumn({ name: "ownerId" })
-  owner: User;
+    @ManyToOne(() => User, (user) => user.apps, { onDelete: "CASCADE" })
+    @JoinColumn({ name: "ownerId" })
+    owner: User;
 }
--- backend/src/features/media/dtos/request.ts
+++ backend/src/features/media/dtos/request.ts
@@ -1,17 +1,13 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { PaginationQuery, RequestWithId } from '@domain/common/dtos/request.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-import {
-  PaginationQuery,
-  RequestWithId,
-} from "@domain/common/dtos/request.dto";
-
-export class GetMediaRequest extends PaginationQuery {}
+export class GetMediaRequest extends PaginationQuery { }
 
 export class CreateMediaRequest {
-  @ApiProperty({ type: String, format: "binary", required: true })
-  file: Express.Multer.File;
+    @ApiProperty({ type: String, format: 'binary', required: true })
+    file: Express.Multer.File
 }
 
-export class DeleteMediaRequest extends RequestWithId {}
+export class DeleteMediaRequest extends RequestWithId { }
 
-export class UpdateMediaRequest extends RequestWithId {}
+export class UpdateMediaRequest extends RequestWithId { }
