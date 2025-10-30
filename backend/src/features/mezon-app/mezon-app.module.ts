3294a9e1a8c58d07c4a6f54b32c8d14ca767419b
Duke <tuoc31082004@gmail.com>
2025-10-30 14:20:16 +0700

    MTB-269-commit

--- backend/src/features/mezon-app/mezon-app.module.ts
+++ backend/src/features/mezon-app/mezon-app.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { App, AppVersion, Link, LinkType, Rating, Tag, User } from "@domain/entities";
 
+import { AppVersionModule } from "@features/app-version/app-version.module";
+
 import { MezonAppController } from "./mezon-app.controller";
 import { MezonAppService } from "./mezon-app.service";
 
-
 @Module({
-  imports: [TypeOrmModule.forFeature([App, Tag, User, Rating, Link, LinkType, AppVersion])],
+  imports: [TypeOrmModule.forFeature([App, Tag, User, Rating, Link, LinkType, AppVersion]), AppVersionModule],
   providers: [MezonAppService],
   controllers: [MezonAppController],
   exports: [MezonAppService],
