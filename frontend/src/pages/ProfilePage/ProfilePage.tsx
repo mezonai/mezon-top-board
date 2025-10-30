a1a33ef6bddc88558f9a80fa6bb58d5a8c697c84
hathuanihi <hathu20905@gmail.com>
2025-10-30 11:49:39 +0700

    feat: add search, pagination, filter  my mezon bots, delete global search in profile page

--- frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,75 +1,89 @@
 import CompactBotCard from '@app/components/CompactBotCard/CompactBotCard'
-import { useAuth } from '@app/hook/useAuth'
 import useAuthRedirect from '@app/hook/useAuthRedirect'
-import { useMezonAppSearch } from '@app/hook/useSearch'
 import Button from '@app/mtb-ui/Button'
 import SearchBar from '@app/mtb-ui/SearchBar/SearchBar'
 import MtbTypography from '@app/mtb-ui/Typography/Typography'
-import { useLazyMezonAppControllerGetMyAppQuery, useLazyMezonAppControllerSearchMezonAppQuery } from '@app/services/api/mezonApp/mezonApp'
+import { useLazyMezonAppControllerSearchMezonAppQuery } from '@app/services/api/mezonApp/mezonApp'
 import { useLazyTagControllerGetTagsQuery } from '@app/services/api/tag/tag'
 import { GetPublicProfileResponse, useLazyUserControllerGetPublicProfileQuery } from '@app/services/api/user/user'
 import { RootState } from '@app/store'
 import { useAppSelector } from '@app/store/hook'
 import { IMezonAppStore } from '@app/store/mezonApp'
 import { IUserStore } from '@app/store/user'
-import { Divider } from 'antd'
-import { useEffect, useState } from 'react'
+import { Divider, Pagination, Flex } from 'antd'
+import { useEffect, useState, useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate, useParams } from 'react-router-dom'
 import { CardInfo } from './components'
+import { MezonAppType } from '@app/enums/mezonAppType.enum'
+import SingleSelect, { IOption } from '@app/mtb-ui/SingleSelect'
+
+const pageOptions = [
+  { value: 9, label: "9 bots/page" },
+  { value: 12, label: "12 bots/page" },
+  { value: 15, label: "15 bots/page" },
+];
 
 function ProfilePage() {
   const navigate = useNavigate()
-  const { isLogin } = useAuth()
   const { userInfo: myInfo, publicProfile: publicUserInfo } = useAppSelector<RootState, IUserStore>((s) => s.user)
   const { userId } = useParams()
-  const { handleSearch } = useMezonAppSearch(1, 5)
   const [getTagList] = useLazyTagControllerGetTagsQuery()
-  const [getMyApp] = useLazyMezonAppControllerGetMyAppQuery()
   const [getMezonApp] = useLazyMezonAppControllerSearchMezonAppQuery()
   const [queryGetPublicProfile] = useLazyUserControllerGetPublicProfileQuery()
   const { mezonApp: userMezonApp } = useSelector<RootState, IMezonAppStore>((s) => s.mezonApp)
-  const { mezonAppOfUser: myMezonApp } = useSelector<RootState, IMezonAppStore>((s) => s.mezonApp)
   const [userInfo, setUserInfo] = useState<GetPublicProfileResponse>()
-  const mezonApp = userId ? userMezonApp : myMezonApp
+  const [searchText, setSearchText] = useState<string>('')
+  const [selectedTagIds, setSelectedTagIds] = useState<string[]>([])
+  const [selectedType, setSelectedType] = useState<MezonAppType | undefined>()
+  const [page, setPage] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(pageOptions[0].value);
+  const mezonApp = userMezonApp
+  const totals = useMemo(() => mezonApp?.totalCount || 0, [mezonApp]);
+  const totalPages = useMemo(() => mezonApp?.totalPages || 0, [mezonApp]);
+  const ownerId = userId ? userId : myInfo?.id;
 
-  const getData = async () => {
-    if (userId) {
-      getMezonApp({
-        pageNumber: 1,
-        pageSize: 100,
-        sortField: 'createdAt',
-        sortOrder: 'DESC',
-        ownerId: userId
-      })
-      return;
-    }
+  const getData = async (
+    query: string = searchText,
+    tags: string[] = selectedTagIds,
+    type: MezonAppType | undefined = selectedType,
+    pageNumber: number = page
+  ) => {
+    if (!ownerId) return;
 
-    if (isLogin) {
-      getMyApp({
-        pageNumber: 1,
-        pageSize: 100,
-        sortField: 'createdAt',
-        sortOrder: 'DESC'
-      })
-    }
+    getMezonApp({
+      pageNumber: pageNumber,
+      pageSize: pageSize,
+      sortField: 'createdAt',
+      sortOrder: 'DESC',
+      ownerId: ownerId,
+      search: query,
+      tags: tags,
+      type: type,
+    });
   }
 
-  const initRequests = async () => {
-    await getTagList()
+  useEffect(() => {
+    getTagList();
+
     if (userId) {
       queryGetPublicProfile({ userId }).unwrap();
     }
-    getData()
-  }
+  }, [userId]);
+
+  useEffect(() => {
+    if (ownerId) {
+      setSearchText('');
+      setSelectedTagIds([]);
+      setSelectedType(undefined);
+      setPage(1);
+      getData('', [], undefined, 1);
+    }
+  }, [ownerId]);
 
   if (!userId) {
     useAuthRedirect()
   }
-
-  useEffect(() => {
-    initRequests()
-  }, [])
 
   useEffect(() => {
     if (!userId) {
@@ -82,43 +96,107 @@ function ProfilePage() {
     }
   }, [myInfo, publicUserInfo]);
 
+  const handleProfileSearch = (
+    query: string | undefined,
+    tags: string[] | undefined,
+    type?: MezonAppType
+  ) => {
+    const searchQuery = query || '';
+    const searchTags = tags || [];
+
+    setSearchText(searchQuery);
+    setSelectedTagIds(searchTags);
+    setSelectedType(type);
+
+    setPage(1);
+    getData(searchQuery, searchTags, type, 1);
+  }
+
+  const handlePageChange = (newPage: number) => {
+    setPage(newPage);
+    getData(searchText, selectedTagIds, selectedType, newPage);
+  };
+
+  const handlePageSizeChange = (option: IOption) => {
+    setPageSize(Number(option.value));
+    setPage(1);
+    getData(searchText, selectedTagIds, selectedType, 1);
+  };
+
   return (
     <div className='pt-8 pb-12 w-[75%] m-auto'>
-      <MtbTypography variant='h1'>Explore millions of Mezon Bots</MtbTypography>
-      <div className='pt-3'>
-        <SearchBar onSearch={(val, tagIds) => handleSearch(val ?? '', tagIds)} isResultPage={false}></SearchBar>
-      </div>
+      <MtbTypography variant='h1'>Your Profile</MtbTypography>
       <Divider className='bg-gray-100'></Divider>
       <div className='flex justify-between gap-15 max-lg:flex-col max-2xl:flex-col'>
         <div className='w-1/3 max-lg:w-full max-2xl:w-full'>
           <CardInfo userInfo={userInfo} isPublic={Boolean(userId)}></CardInfo>
         </div>
+
         <div className='flex-2'>
-          <div className='flex justify-between items-center pb-10'>
-            <MtbTypography variant='h2'>Welcome to {userId ? `${userInfo?.name}'s` : 'your'} profile</MtbTypography>
+          <div className='flex justify-between items-center pb-4'>
+            <MtbTypography variant='h2'>My Mezon Bots</MtbTypography>
             {!userId && (
               <Button color='primary' size='large' onClick={() => navigate('/new-bot')}>
                 Add new bot
               </Button>
             )}
           </div>
-          <div className='grid grid-cols-1 gap-8 min-lg:grid-cols-2 min-xl:grid-cols-3 max-w-full'>
-            {mezonApp?.data?.map((item) => <CompactBotCard key={item.id} data={item} isPublic={Boolean(userId)}></CompactBotCard>)}
-          </div>
-          {/* TODO: Add pagination */}
-          {/* <div className='flex justify-center pt-10'>
-            <Button
-              color='primary'
-              size='large'
-              variant='outlined'
-              onClick={() => {
-                setCurrentPageNumber((prev) => prev + 1)
-                getData(true)
-              }}
-            >
-              Load more
-            </Button>
-          </div> */}
+
+          <SearchBar
+            onSearch={handleProfileSearch}
+            isResultPage={true}
+            isShowButton={false}
+          />
+
+          {totalPages !== 0 && (
+            <div className='pt-8'>
+              <Flex justify="space-between" wrap="wrap">
+                <div className='flex-shrink-0'>
+                  <MtbTypography variant='h5' weight='normal'>
+                    Showing {page} of {totalPages} page
+                  </MtbTypography>
+                </div>
+                <Flex gap={10} align='center' wrap="wrap">
+                  <SingleSelect
+                    getPopupContainer={(trigger) => trigger.parentElement}
+                    onChange={handlePageSizeChange}
+                    options={pageOptions}
+                    placeholder='Select'
+                    size='large'
+                    className='w-[10rem]'
+                    dropDownTitle='Bots per page'
+                    value={pageOptions.find(o => o.value === pageSize)}
+                  />
+                </Flex>
+              </Flex>
+            </div>
+          )}
+
+          {totalPages === 0 ? (
+            <div className='pt-8'>
+              <div className='text-center py-12 text-gray-500'>No Result</div>
+            </div>
+          ) : (
+            <>
+              <div className='grid grid-cols-1 gap-8 min-lg:grid-cols-2 min-xl:grid-cols-3 max-w-full pt-8'>
+                {mezonApp?.data?.map((item) => (
+                  <CompactBotCard key={item.id} data={item} isPublic={Boolean(userId)} />
+                ))}
+              </div>
+
+              <div className='flex flex-col items-center gap-5 pt-10'>
+                <div className='flex flex-col items-center relative w-full'>
+                  <Pagination
+                    onChange={handlePageChange}
+                    pageSize={pageSize}
+                    showSizeChanger={false}
+                    current={page}
+                    total={totals}
+                  />
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
