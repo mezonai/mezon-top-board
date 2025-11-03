df5f67d09c0675b01c66a047c3e7b6b01ba18227
Duke <tuoc31082004@gmail.com>
2025-11-03 10:12:16 +0700

    remove-whitespace

--- backend/src/features/app-version/dtos/request.ts
+++ backend/src/features/app-version/dtos/request.ts
@@ -4,6 +4,7 @@ import { IsOptional, IsString } from "class-validator";
 
 import { CreateAppInfoRequest } from "@domain/common/dtos/appInfo.dto";
 
+
 export class CreateAppVersionRequest extends PartialType(CreateAppInfoRequest) {
   @ApiProperty()
   @IsString()
