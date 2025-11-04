b51cbd36cce45c844ece57a013259db9f044999e
Duke <tuoc31082004@gmail.com>
2025-11-04 11:36:48 +0700

    MTB-272-update-migrations

--- backend/src/migrations/1761621006644-add-enum-app-version.ts
+++ backend/src/migrations/1761621006644-add-enum-app-version.ts
@@ -1,11 +1,12 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddEnumAppVersion1761425901830 implements MigrationInterface {
-    name = 'AddEnumAppVersion1761425901830'
+export class AddEnumAppVersion1761621006644 implements MigrationInterface {
+    name = 'AddEnumAppVersion1761621006644'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "app" ADD "currentVersion" integer NOT NULL DEFAULT '1'`);
         await queryRunner.query(`ALTER TABLE "app" ADD "hasNewUpdate" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`CREATE TYPE "public"."app_version_type_enum" AS ENUM('app', 'bot')`);
         await queryRunner.query(`CREATE TYPE "public"."app_version_status_enum" AS ENUM('0', '1', '2', '3')`);
         await queryRunner.query(`CREATE TYPE "public"."app_version_pricingtag_enum" AS ENUM('FREE', 'PAID')`);
     }
