44d18577e65d5d1a26df94ff44356b6e5bc8d7ec
Duke <tuoc31082004@gmail.com>
2025-12-03 17:33:27 +0700

    MTB-297-commit-update-temp-storage-helper

--- backend/src/features/job/bot-generator.job.ts
+++ backend/src/features/job/bot-generator.job.ts
@@ -6,10 +6,9 @@ import { TempSourceFile } from '@domain/entities';
 import { EntityManager } from 'typeorm';
 import * as fs from 'fs-extra';
 import * as path from 'path';
-import * as archiver from 'archiver';
-import * as Handlebars from 'handlebars';
 import { TempSourceFileStatus } from '@domain/common/enum/tempSourceFileStatus';
-import { BotWizardRequest, CommandWizardRequest } from '@features/bot-generator/dtos/request';
+import { BotWizardRequest } from '@features/bot-generator/dtos/request';
+import { TempStorageHelper } from '@libs/utils/tempStorageHelper';
 
 @Injectable()
 export class BotGeneratorJob implements OnModuleInit {
@@ -43,17 +42,33 @@ export class BotGeneratorJob implements OnModuleInit {
     const tempOutput = path.join('/tmp', `bot-${Date.now()}`);
 
     await fs.mkdirp(tempOutput);
-    await this.renderDirectory(templateRoot, tempOutput, payload);
-    if (Array.isArray(payload.commands) && payload.commands.length > 0) {
-      const commandsDir = path.join(tempOutput, 'command');
-      await fs.mkdirp(commandsDir);
 
-      for (const cmd of payload.commands) {
-        await this.generateCommandFile(commandsDir, cmd);
+    await TempStorageHelper.renderDirectory(templateRoot, tempOutput, payload);
+
+    const commandTemplate = `
+      import { ChannelMessage } from 'mezon-sdk';
+      import { Command } from '@app/decorators/command.decorator';
+      import { CommandMessage } from '@app/command/common/command.abstract';
+
+      @Command('{{command}}', {
+        description: '{{description}}',
+        usage: '!{{command}}',
+        category: '{{category}}',
+        aliases: [{{#each aliases}}"{{this}}",{{/each}}]
+      })
+      export class {{className}} extends CommandMessage {
+        execute(args: string[], message: ChannelMessage) {
+          const msg = \`{{command}} executed!\`;
+          return this.replyMessageGenerate({ messageContent: msg }, message);
+        }
       }
+    `;
+
+    if (Array.isArray(payload.commands) && payload.commands.length > 0) {
+      await TempStorageHelper.generateCommandFiles(tempOutput, payload.commands, commandTemplate);
     }
 
-    const zipBuffer = await this.zipFolder(tempOutput);
+    const zipBuffer = await TempStorageHelper.zipFolder(tempOutput);
     await fs.remove(tempOutput);
 
     const tempFolder = path.join('/tmp/bot-source-files', tempFileId);
@@ -76,84 +91,5 @@ export class BotGeneratorJob implements OnModuleInit {
 
   async addToQueue(data: BotGeneratorJobData) {
     return await this.queue.send<BotGeneratorJobData>('bot-generator-queue', data);
-  }
-
-  private async renderDirectory(srcDir: string, destDir: string, payload: BotWizardRequest) {
-    const items = await fs.readdir(srcDir);
-
-    for (const item of items) {
-      const srcPath = path.join(srcDir, item);
-      let destPath = path.join(destDir, item);
-
-      const stat = await fs.stat(srcPath);
-
-      if (stat.isDirectory()) {
-        await fs.mkdirp(destPath);
-        await this.renderDirectory(srcPath, destPath, payload);
-      }
-
-      else if (item.endsWith('.hbs')) {
-        const realName = item.replace('.hbs', '');
-        const templateContent = await fs.readFile(srcPath, 'utf8');
-        const template = Handlebars.compile(templateContent);
-        const rendered = template(payload);
-        destPath = path.join(destDir, realName);
-        await fs.writeFile(destPath, rendered);
-      }
-
-      else {
-        await fs.copyFile(srcPath, destPath);
-      }
-    }
-  }
-
-  private async zipFolder(folderPath: string): Promise<Buffer> {
-    const zipPath = `${folderPath}.zip`;
-
-    return new Promise((resolve, reject) => {
-      const output = fs.createWriteStream(zipPath);
-      const archive = archiver('zip', { zlib: { level: 9 } });
-
-      archive.directory(folderPath, false);
-      archive.pipe(output);
-
-      archive.finalize();
-
-      output.on('close', async () => {
-        const data = await fs.readFile(zipPath);
-        await fs.remove(zipPath);
-        resolve(data);
-      });
-
-      archive.on('error', reject);
-    });
-  }
-
-  private async generateCommandFile(commandsDir: string, command: CommandWizardRequest) {
-
-    const template = `
-      import { ChannelMessage } from 'mezon-sdk';
-      import { Command } from '@app/decorators/command.decorator';
-      import { CommandMessage } from '@app/command/common/command.abstract';
-
-      @Command('{{command}}', {
-        description: '{{description}}',
-        usage: '!{{command}}',
-        category: '{{category}}',
-        aliases: [{{#each aliases}}"{{this}}",{{/each}}],
-      })
-      export class {{className}} extends CommandMessage {
-        execute(args: string[], message: ChannelMessage) {
-          const messageContent = \`{{command}} executed!\`; 
-          return this.replyMessageGenerate({ messageContent }, message);
-        }
-      }
-    `;
-
-    const compile = Handlebars.compile(template);
-    const rendered = compile(command);
-
-    const filePath = path.join(commandsDir, `${command.className}.ts`);
-    await fs.writeFile(filePath, rendered);
   }
 }
