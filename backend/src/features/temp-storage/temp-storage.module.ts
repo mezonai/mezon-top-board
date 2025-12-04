6ba5d75a6b91b12d6ab1c2efec218bfb717a1569
Duke <tuoc31082004@gmail.com>
2025-12-04 11:57:05 +0700

    MTB-297-commit-remove-cleanup

--- backend/src/features/temp-storage/temp-storage.module.ts
+++ backend/src/features/temp-storage/temp-storage.module.ts
@@ -2,12 +2,19 @@ import { Module } from '@nestjs/common';
 import { TempStorageService } from './temp-storage.service';
 import { TempStorageController } from './temp-storage.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+<<<<<<< HEAD
 import { TempFile } from '@domain/entities';
+=======
+import { TempSourceFile } from '@domain/entities';
+>>>>>>> cca8f2b (MTB-297-commit-remove-cleanup)
 
 @Module({
   imports: [TypeOrmModule.forFeature([TempFile]),],
   controllers: [TempStorageController],
   providers: [TempStorageService],
+<<<<<<< HEAD
   exports: [TempStorageService],
+=======
+>>>>>>> cca8f2b (MTB-297-commit-remove-cleanup)
 })
 export class TempStorageModule {}
