d6201a01fc305d199f70938eccca043c622e6da1
hathuanihi <hathu20905@gmail.com>
2025-12-08 14:17:00 +0700

    chore

--- frontend/src/components/FormField/FormField.tsx
+++ frontend/src/components/FormField/FormField.tsx
@@ -13,7 +13,7 @@ function FormField({ label, required = false, description = '', children, errorT
   return (
     <div className={`flex items-start pt-10 gap-6 ${customClass}`}>
       <div className='flex flex-col w-1/4'>
-        <p className='text-[17px] sm:text-[18px] text-[var(--text-primary)] font-medium'>{label}</p>
+        <p className='text-[17px] sm:text-[18px] text-[var(--text-primary)] font-medium'>{label}{required && <span className="text-red-500 ml-1">*</span>}</p>
         <p className='text-[13px] sm:text-[14px] text-[var(--text-secondary)] mt-1'>{description}</p>
       </div>
       <div className='flex flex-col w-[calc(75%-1.5rem)]'>
