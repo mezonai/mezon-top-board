50140b51c7731597bc9e9b60adc23bc387464d58
Duke <tuoc31082004@gmail.com>
2025-12-12 17:49:14 +0700

    MTB-307-commit-update-get-integrations

--- backend/src/features/bot-generator/bot-generator.controller.ts
+++ backend/src/features/bot-generator/bot-generator.controller.ts
@@ -2,12 +2,15 @@ import {
   Controller,
   Post,
   Body,
+  Get,
+  Param,
 } from '@nestjs/common';
 import { BotGeneratorService } from '@features/bot-generator/bot-generator.service';
 import { BotWizardRequest } from '@features/bot-generator/dtos/request';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
 import { GetUserFromHeader } from '@libs/decorator/getUserFromHeader.decorator';
 import { User } from '@domain/entities';
+import { OAuth2Request } from '@features/auth/dtos/request';
 
 @Controller('bot-generator')
 @ApiTags('BotGenerator')
@@ -17,7 +20,14 @@ export class BotGeneratorController {
 
   @Post()
   @ApiBearerAuth()
+  @ApiBody({ type: OAuth2Request })
   async createJob(@Body() payload: BotWizardRequest, @GetUserFromHeader() user: User) {
     return await this.service.genBotTemplate(payload, user.id);
+  }
+
+  @Get('integrations/:language')
+  @ApiBearerAuth()
+  async getIntegrationsList(@Param('language') language: string) {
+    return await this.service.getIntegrationsList(language);
   }
 }
