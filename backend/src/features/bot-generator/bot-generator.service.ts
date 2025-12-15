ef04d88d4385206dd413a8c708511122ba25f285
Duke <tuoc31082004@gmail.com>
2025-12-15 13:10:51 +0700

    MTB-307-commit-add-event-code-engine (#315)
    * MTB-307-commit-add-event-code-engine
    
    * MTB-307-commit-update-render-listener-files
    
    * MTB-307-commit-update-class-name-event
    
    * MTB-307-commit-minimize-change
    
    * MTB-307-commit-update-get-integrations
    
    * MTB-307-commit-fixed-import-fs

--- backend/src/features/bot-generator/bot-generator.service.ts
+++ backend/src/features/bot-generator/bot-generator.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { BotGeneratorJob } from '@features/job/bot-generator.job';
 import { BotWizardRequest } from '@features/bot-generator/dtos/request';
+import { join } from 'path';
+import { promises } from 'fs';
 
 @Injectable()
 export class BotGeneratorService {
-  constructor(private readonly botGeneratorJob: BotGeneratorJob,) {}
+  constructor(private readonly botGeneratorJob: BotGeneratorJob,) { }
 
   async genBotTemplate(payload: BotWizardRequest, ownerId: string) {
     return await this.botGeneratorJob.addToQueue({ payload, ownerId });
+  }
+
+  async getIntegrationsList(language: string): Promise<string[]> {
+    const integrationsPath = join(process.cwd(), 'bot-gen-templates', language, 'src', 'integrations');
+
+    const folders = await promises.readdir(integrationsPath, { withFileTypes: true });
+
+    const integrations = folders
+      .filter((folder) => folder.isDirectory())
+      .map((folder) => folder.name);
+
+    return integrations;
   }
 }
