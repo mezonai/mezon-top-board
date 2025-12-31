061bc3678f3c7bcd28c62c1710370eddf5de9317
Quang Dai Thien <daithien2004@gmail.com>
2025-12-31 08:50:10 +0700

    update: separate translation into small file

--- frontend/src/i18n/i18n.ts
+++ frontend/src/i18n/i18n.ts
@@ -2,12 +2,41 @@ import i18n from "i18next";
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import translationEN from "./locales/en/translation.json";
-import translationVI from "./locales/vi/translation.json";
+import commonEN from "./locales/en/common.json";
+import pagesEN from "./locales/en/pages.json";
+import profileEN from "./locales/en/profile.json";
+import botEN from "./locales/en/bot.json";
+import componentsEN from "./locales/en/components.json";
+import validationEN from "./locales/en/validation.json";
+
+import commonVI from "./locales/vi/common.json";
+import pagesVI from "./locales/vi/pages.json";
+import profileVI from "./locales/vi/profile.json";
+import botVI from "./locales/vi/bot.json";
+import componentsVI from "./locales/vi/components.json";
+import validationVI from "./locales/vi/validation.json";
 
 const resources = {
-    en: {translation: translationEN},
-    vi: {translation: translationVI}
+    en: {
+        translation: {
+            ...commonEN,
+            ...pagesEN,
+            ...profileEN,
+            ...botEN,
+            ...componentsEN,
+            ...validationEN
+        }
+    },
+    vi: {
+        translation: {
+            ...commonVI,
+            ...pagesVI,
+            ...profileVI,
+            ...botVI,
+            ...componentsVI,
+            ...validationVI
+        }
+    }
 }
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
