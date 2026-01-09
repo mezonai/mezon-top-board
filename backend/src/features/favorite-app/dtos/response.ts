ddd6b4f37f1188e211f83ed1ba565018dccce015
hathuanihi <hathu20905@gmail.com>
2026-01-09 14:32:17 +0700

    chore

--- backend/src/features/favorite-app/dtos/response.ts
+++ backend/src/features/favorite-app/dtos/response.ts
@@ -1,3 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Expose, Type } from "class-transformer";
 import { GetMezonAppDetailsResponse } from "@features/mezon-app/dtos/response";
+import { TagInMezonAppDetailResponse } from "@features/tag/dtos/response";
 
-export class FavoriteAppResponseDto extends GetMezonAppDetailsResponse { }
+export class FavoriteAppResponseDto extends GetMezonAppDetailsResponse { 
+    @Expose()
+    @ApiProperty({ type: () => [TagInMezonAppDetailResponse] })
+    @Type(() => TagInMezonAppDetailResponse)
+    public tags: TagInMezonAppDetailResponse[];
+}
