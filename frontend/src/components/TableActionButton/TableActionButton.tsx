b3e74eae1bea0b9edacb40fb7163ffb4b469eefd
Quang Dai Thien <daithien2004@gmail.com>
2026-01-12 08:50:10 +0700

    update: move config out of component

--- frontend/src/components/TableActionButton/TableActionButton.tsx
+++ frontend/src/components/TableActionButton/TableActionButton.tsx
@@ -1,7 +1,7 @@
 import { Tooltip } from 'antd';
-import { EyeOutlined, EditOutlined, DeleteOutlined, DownloadOutlined, PlusOutlined, SearchOutlined, UnlockOutlined, LockOutlined } from '@ant-design/icons';
 import Button from '@app/mtb-ui/Button';
 import { ReactNode } from 'react';
+import { actionConfigs } from './action.config';
 
 export type TableActionType = 'view' | 'edit' | 'delete' | 'install' | 'search' | 'add' | 'activate' | 'review';
 
@@ -21,57 +21,6 @@ interface TableActionButtonProps {
   htmlType?: 'button' | 'submit' | 'reset';
   size?: 'small' | 'middle' | 'large';
 }
-
-const actionConfigs = {
-  view: {
-    icon: <EyeOutlined style={{ fontSize: 18 }} />,
-    color: 'default',
-    tooltip: 'View',
-    variant: 'text',
-  },
-  edit: {
-    icon: <EditOutlined style={{ fontSize: 18 }} />,
-    color: 'blue',
-    tooltip: 'Edit',
-    variant: 'text',
-  },
-  delete: {
-    icon: <DeleteOutlined style={{ fontSize: 18 }} />,
-    color: 'danger',
-    tooltip: 'Delete',
-    variant: 'text',
-  },
-  install: {
-    icon: <DownloadOutlined style={{ fontSize: 18 }} />,
-    color: 'cyan',
-    tooltip: 'Try Install',
-    variant: 'text',
-  },
-  search: {
-    icon: <SearchOutlined style={{ fontSize: 18 }} />,
-    color: 'primary',
-    tooltip: '',
-    variant: 'solid',
-  },
-  add: {
-    icon: <PlusOutlined style={{ fontSize: 18 }} />,
-    color: 'primary',
-    tooltip: '',
-    variant: 'solid',
-  },
-  activate: {
-    icon: <UnlockOutlined style={{ fontSize: 18 }} />,
-    color: 'green',
-    tooltip: 'Activate',
-    variant: 'text',
-  },
-  review: {
-    icon: <LockOutlined style={{ fontSize: 18 }} />,
-    color: 'cyan',
-    tooltip: 'Review',
-    variant: 'text',
-  },
-};
 
 const TableActionButton = ({
   actionType,
