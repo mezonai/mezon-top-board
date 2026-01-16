ffdc365375b69c0721eee94ecb278a24d9f096d4
Quang Dai Thien <daithien2004@gmail.com>
2026-01-16 09:01:42 +0700

    update: migration for color

--- backend/src/migrations/1768528868557-add-color-for-tag.ts
+++ backend/src/migrations/1768528868557-add-color-for-tag.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddColorForTag1768463580697 implements MigrationInterface {
-    name = 'AddColorForTag1768463580697'
+export class AddColorForTag1768528868557 implements MigrationInterface {
+    name = 'AddColorForTag1768528868557'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "tag" ADD "color" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "tag" ADD "color" character varying NOT NULL DEFAULT 'magenta'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
